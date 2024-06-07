662d790fb6e6ee0b0b7337e36dfe996ae86c1bf2
sohanmatsa <sohanmatsa1@gmail.com>
2024-06-07 22:55:51 +0530

    Added javascript code. All good !

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+    "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6",
+    "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[",
+    "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
+
+
+let generatedPasswd = document.getElementById("passwd-output")
+
+
+function generatePasswd() {
+
+    let password = ""
+    const length = 12
+
+    while (length > password.length) {
+        password += characters[Math.floor(Math.random() * characters.length)]
+    }
+    // @ts-ignore
+    generatedPasswd.value = password
+}
+
+function copyPassword(){
+    // @ts-ignore
+    generatedPasswd.select()
+    navigator.clipboard.writeText(generatedPasswd.value);
+    
+}
