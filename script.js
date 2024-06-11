0036b62808ab647fbc8d801005f7765c61a33b51
sohanmatsa <sohanmatsa1@gmail.com>
2024-06-11 16:41:34 +0530

    Added password length option to the app

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
     "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6",
-    "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[",
-    "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+    "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "-", "+", "=", ",", ":", ";", "<", ">", ".", "?", "/"];
 
 
-let generatedPasswd = document.getElementById("passwd-output")
+const generatedPasswd = document.getElementById("passwd-output")
+const rangeEl = document.getElementById("range")
+const sliderNumberEl = document.getElementById("slider-number")
 
+function updateSlider() {
+    sliderNumberEl.textContent = rangeEl.value
+}
 
 function generatePasswd() {
 
     let password = ""
-    const length = 12
+    const length = rangeEl.value
 
     while (length > password.length) {
         password += characters[Math.floor(Math.random() * characters.length)]
